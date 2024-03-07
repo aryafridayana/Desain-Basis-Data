fb05415b264bb4e0604e80351b4d0f1d9598ed20
aryafridayana <160809364+aryafridayana@users.noreply.github.com>
2024-03-07 10:18:11 +0700

    Add files via upload

---
+++ HTML.CSS/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$databaseHost = 'localhost';
+$username = 'root';
+$password = '';
+$databaseName = 'basisdata';
+
+$mysqli = mysqli_connect ($databaseHost, $username, $password,$databaseName);
+
+?>
+
+
+
+
+
+
+
